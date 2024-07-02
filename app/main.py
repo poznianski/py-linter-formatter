06304d099f32b1c573e55aaab5d6acc8a944d988
poznianski <poznianski@gmail.com>
2024-07-02 18:57:03 +0000

    optimized

--- app/main.py
+++ app/main.py
@@ -10,15 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            } for error in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
         "status": "failed" if errors else "passed"
     }
